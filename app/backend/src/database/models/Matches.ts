fba38744de57fa725d49c9420121add69ad4a69a
Ricardo <rbrumjunio@gmail.com>
2023-05-25 05:49:37 -0300

    Adicionando model de Matches

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,49 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '.';
+import Teams from './Teams';
+
+export default class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  tableName: 'matches',
+  timestamps: false,
+  sequelize,
+  underscored: true,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeamId', as: 'awayTeam' });
